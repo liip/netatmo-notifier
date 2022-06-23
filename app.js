33ab7d0f2e4c30576df5871139382722862ef15e
Sylvain Nicolet <nicoletsylvain@gmail.com>
2022-06-23 10:18:36 +0200

    Issue #11 : Install eslint, apply rules

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
-const Netatmo = require('./services/Netatmo')
-const express = require('express');
+require("dotenv").config();
+const Netatmo = require("./services/Netatmo");
+const express = require("express");
 
 // Netatmo authentication
 const auth = {
-	client_id: process.env.NETATMO_CLIENT_ID,
-	client_secret: process.env.NETATMO_CLIENT_SECRET,
-	username: process.env.NETATMO_USERNAME,
-	password: process.env.NETATMO_PASSWORD,
-	scope: 'read_smokedetector',
-	grant_type: 'password',
+  client_id: process.env.NETATMO_CLIENT_ID,
+  client_secret: process.env.NETATMO_CLIENT_SECRET,
+  username: process.env.NETATMO_USERNAME,
+  password: process.env.NETATMO_PASSWORD,
+  scope: "read_smokedetector",
+  grant_type: "password",
 };
 
 // Netatmo webhook
-const webhook = process.env.APP_URL + '/webhook';
+const webhook = process.env.APP_URL + "/webhook";
 
 // Authenticate and set webhook
 let netatmo = new Netatmo();
-netatmo.addWebhook(auth, webhook)
+netatmo.addWebhook(auth, webhook);
 
 // Create express server
 const app = express();
@@ -25,11 +25,11 @@ app.use(express.json());
 const port = 80;
 
 // Webhook route
-app.post('/webhook', function (request, response) {
-	response.send(request.body);
+app.post("/webhook", function (request, response) {
+  response.send(request.body);
 });
 
 // Start server
 app.listen(port, () => {
-	console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
--- services/Netatmo.js
+++ services/Netatmo.js
@@ -1,71 +1,71 @@
-require('dotenv').config();
-const util = require('util');
-const qs = require('qs');
-const axios = require('axios');
+require("dotenv").config();
+const util = require("util");
+const qs = require("qs");
+const axios = require("axios");
 const BASE_URL = process.env.NETATMO_BASE_URL;
 
 class Netatmo {
 
-	/**
-	 * @constructor
-	 */
-	constructor() {
-	}
+  /**
+   * @constructor
+   */
+  constructor() {
+  }
 
-	/**
-	 * Add webhook
-	 * @param args
-	 * @param webhook
-	 */
-	addWebhook(args, webhook) {
-		const data = {
-			client_id: args.client_id,
-			client_secret: args.client_secret,
-			username: args.username,
-			password: args.password,
-			scope: args.scope,
-			grant_type: args.grant_type,
-		};
+  /**
+   * Add webhook
+   * @param args
+   * @param webhook
+   */
+  addWebhook(args, webhook) {
+    const data = {
+      client_id: args.client_id,
+      client_secret: args.client_secret,
+      username: args.username,
+      password: args.password,
+      scope: args.scope,
+      grant_type: args.grant_type,
+    };
 
-		let url = util.format('%s/oauth2/token', BASE_URL);
+    let url = util.format("%s/oauth2/token", BASE_URL);
 
-		const options = {
-			method: 'POST',
-			headers: { 'content-type': 'application/x-www-form-urlencoded' },
-			data: qs.stringify(data),
-			url,
-		};
+    const options = {
+      method: "POST",
+      headers: {"content-type": "application/x-www-form-urlencoded"},
+      data: qs.stringify(data),
+      url,
+    };
 
-		axios(options)
-			.then(function (response) {
-				console.log(response.data);
+    axios(options)
+      .then(function (response) {
+        console.log(response.data);
 
-				let access_token = response.data.access_token;
-				let url = util.format('%s/api/addwebhook', BASE_URL);
+        let access_token = response.data.access_token;
+        let url = util.format("%s/api/addwebhook", BASE_URL);
 
-				const options = {
-					method: 'POST',
-					headers: {
-						'Authorization': `Bearer ${access_token}`
-					},
-					data: {
-						url: webhook
-					},
-					url,
-				};
+        const options = {
+          method: "POST",
+          headers: {
+            "Authorization": `Bearer ${access_token}`
+          },
+          data: {
+            url: webhook
+          },
+          url,
+        };
 
-				axios(options)
-					.then(function (response) {
-						console.log(response.data);
-					})
-					.catch(function (error) {
-						console.log(error);
-					});
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
-	}
+        axios(options)
+          .then(function (response) {
+            console.log(response.data);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 }
 
 module.exports = Netatmo;
