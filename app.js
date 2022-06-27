ea4865d5bace91d226f3c87ab29b489d5c92ef54
Sylvain Nicolet <nicoletsylvain@gmail.com>
2022-06-27 22:50:39 +0200

    Issue #9 : Add es lint script, change rules and apply

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
-require("dotenv").config();
-const NetatmoService = require("./services/Netatmo");
-const express = require("express");
-const netatmo = require('netatmo');
+require('dotenv').config()
+const NetatmoService = require('./services/Netatmo')
+const express = require('express')
+const netatmo = require('netatmo')
 
 // Netatmo webhook
-const WEBHOOK = process.env.APP_URL + "/webhook";
+const WEBHOOK = process.env.APP_URL + '/webhook'
 
 // Authenticate and set webhook
-let netatmoService = new NetatmoService();
-netatmoService.addWebhook(WEBHOOK);
+let netatmoService = new NetatmoService()
+netatmoService.addWebhook(WEBHOOK)
 
 // Create express server
-const app = express();
-app.use(express.json());
-const port = 80;
+const app = express()
+app.use(express.json())
+const port = 80
 
 // Webhook route
-app.post("/webhook", function (request, response) {
+app.post('/webhook', function (request, response) {
 
-  let homeId = request.body.home_id;
-  let deviceId = request.body.device_id;
-  let eventType = request.body.event_type;
-  let subType = request.body.sub_type;
-  let eventDescription = getEventDescription(eventType, subType);
+  let homeId = request.body.home_id
+  let deviceId = request.body.device_id
+  let eventType = request.body.event_type
+  let subType = request.body.sub_type
+  let eventDescription = getEventDescription(eventType, subType)
 
   let auth = {
     client_id: process.env.NETATMO_CLIENT_ID,
     client_secret: process.env.NETATMO_CLIENT_SECRET,
     username: process.env.NETATMO_USERNAME,
     password: process.env.NETATMO_PASSWORD,
-  };
+  }
 
-  let api = new netatmo(auth);
+  let api = new netatmo(auth)
 
   let getHomeData = function(err, data) {
 
-    let homeName = data.homes[0].name;
-    let deviceName = getDevice(deviceId, data.homes[0].smokedetectors).name;
+    let homeName = data.homes[0].name
+    let deviceName = getDevice(deviceId, data.homes[0].smokedetectors).name
 
-    console.log(`Home: ${homeName}\nDevice: ${deviceName}\nEvent Type: ${eventType}\nDescription: ${eventDescription}`);
-  };
+    console.log(`Home: ${homeName}\nDevice: ${deviceName}\nEvent Type: ${eventType}\nDescription: ${eventDescription}`)
+  }
 
   // Event Listeners
-  api.on('get-homedata', getHomeData);
+  api.on('get-homedata', getHomeData)
 
   // Get home data
-  api.getHomeData();
+  api.getHomeData()
 
-  response.send(request.body);
-});
+  response.send(request.body)
+})
 
 // Start server
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+  console.log(`App listening on port ${port}`)
+})
 
 /**
  * Get event description
@@ -62,91 +62,91 @@ function getEventDescription(eventType, subType) {
 
   if (eventType === 'battery_status') {
     if (subType === 0) {
-      return "Low"
+      return 'Low'
     }
     else if (subType === 1) {
-      return "Very Low"
+      return 'Very Low'
     }
     else {
-      return "Unknown subtype"
+      return 'Unknown subtype'
     }
   }
 
   if (eventType === 'detection_chamber_status') {
     if (subType === 0) {
-      return "Clean"
+      return 'Clean'
     }
     else if (subType === 1) {
-      return "Dusty"
+      return 'Dusty'
     }
     else {
-      return "Unknown subtype"
+      return 'Unknown subtype'
     }
   }
 
   if (eventType === 'hush') {
     if (subType === 0) {
-      return "Activate"
+      return 'Activate'
     }
     else {
-      return "Unknown subtype"
+      return 'Unknown subtype'
     }
   }
 
   if (eventType === 'smoke') {
     if (subType === 0) {
-      return "Cleared"
+      return 'Cleared'
     }
     else if (subType === 1) {
-      return "Detected"
+      return 'Detected'
     }
     else {
-      return "Unknown subtype"
+      return 'Unknown subtype'
     }
   }
 
   if (eventType === 'sound_test') {
     if (subType === 0) {
-      return "Ok"
+      return 'Ok'
     }
     else if (subType === 1) {
-      return "Error"
+      return 'Error'
     }
     else {
-      return "Unknown subtype"
+      return 'Unknown subtype'
     }
   }
 
   if (eventType === 'tampered') {
     if (subType === 0) {
-      return "Ready"
+      return 'Ready'
     }
     else if (subType === 1) {
-      return "Tampered"
+      return 'Tampered'
     }
     else {
-      return "Unknown subtype"
+      return 'Unknown subtype'
     }
   }
 
   if (eventType === 'wifi_status') {
     if (subType === 0) {
-      return "Error"
+      return 'Error'
     }
     else if (subType === 1) {
-      return "Ok"
+      return 'Ok'
     }
     else {
-      return "Unknown subtype"
+      return 'Unknown subtype'
     }
   }
 
-  return "Unknown event"
+  return 'Unknown event'
 }
 
 /**
  * Get device
  */
 function getDevice(deviceId, devices) {
-  return devices.find(device => device.id === deviceId);
+  return devices.find(device => device.id === deviceId)
 }
--- services/Netatmo.js
+++ services/Netatmo.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const util = require("util");
-const qs = require("qs");
-const axios = require("axios");
-const BASE_URL = process.env.NETATMO_BASE_URL;
+require('dotenv').config()
+const util = require('util')
+const qs = require('qs')
+const axios = require('axios')
+const BASE_URL = process.env.NETATMO_BASE_URL
 
 class Netatmo {
 
@@ -19,31 +19,31 @@ class Netatmo {
   addWebhook(webhook) {
     this.authenticate()
       .then(function (response) {
-        let access_token = response.data.access_token;
-        let url = util.format("%s/api/addwebhook", BASE_URL);
+        let access_token = response.data.access_token
+        let url = util.format('%s/api/addwebhook', BASE_URL)
 
         const options = {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Authorization": `Bearer ${access_token}`
+            'Authorization': `Bearer ${access_token}`
           },
           data: {
             url: webhook
           },
           url,
-        };
+        }
 
         axios(options)
           .then(function (response) {
-            // console.log(response.data);
+            // console.log(response.data)
           })
           .catch(function (error) {
-            console.log(error);
-          });
+            console.log(error)
+          })
       })
       .catch(function (error) {
-        console.log(error);
-      });
+        console.log(error)
+      })
   }
 
   /**
@@ -55,21 +55,21 @@ class Netatmo {
       client_secret: process.env.NETATMO_CLIENT_SECRET,
       username: process.env.NETATMO_USERNAME,
       password: process.env.NETATMO_PASSWORD,
-      scope: "read_smokedetector",
-      grant_type: "password",
-    };
+      scope: 'read_smokedetector',
+      grant_type: 'password',
+    }
 
-    let url = util.format("%s/oauth2/token", BASE_URL);
+    let url = util.format('%s/oauth2/token', BASE_URL)
 
     const options = {
-      method: "POST",
-      headers: {"content-type": "application/x-www-form-urlencoded"},
+      method: 'POST',
+      headers: {'content-type': 'application/x-www-form-urlencoded'},
       data: qs.stringify(data),
       url,
-    };
+    }
 
-    return axios(options);
+    return axios(options)
   }
 }
 
-module.exports = Netatmo;
+module.exports = Netatmo
